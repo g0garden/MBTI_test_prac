b202e92c2466b2a20f91968b320d4a9e4095984f
g0garden <58612140+g0garden@users.noreply.github.com>
2021-06-28 23:33:33 +0900

    view:result 구현

--- js/start.js
+++ js/start.js
@@ -1,20 +1,65 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const result = document.querySelector("#result");
+const endPoint = 12;
 
-//data에서 q순서에 맞는 answer들 버튼으로 보여주기
-function addAnswer(answerText){
-  let a = document.querySelector('.answerBox');
-  let answer = document.createElement('button');
-  a.appendChild(answer)
-  answer.innerHTML = answerText;
+function goResult(){
+  qna.style.WebkitAnimation = "fadeOut 1s";
+  qna.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    result.style.WebkitAnimation="fadeIn 1s";
+    result.style.animation ="fadeIn 1s";
+    
+    setTimeout(()=>{
+      qna.style.display = "none";
+      result.style.display="block";
+    }, 450)})
 }
 
+
+//data에서 q순서에 맞는 answer들 버튼으로 보여주기
+function addAnswer(answerText, qIdx){
+  let a = document.querySelector('.answerBox');
+  let answer = document.createElement('button');
+  answer.classList.add('answerList'); //버튼만들면서 클래스명도 같이 넣어주렴 그래야 나중에 이벤트등록하지!
+  answer.classList.add('my-5');
+  answer.classList.add('py-3');
+  answer.classList.add('mx-auto');
+  answer.classList.add('fadeIn');
+
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+
+  answer.addEventListener("click", function(){
+    let childrenBtn = document.querySelectorAll('.answerList');
+    for(let i = 0; i < childrenBtn.length; i++){
+      childrenBtn[i].disabled = true;
+      childrenBtn[i].style.animation = "fadeOut";
+      childrenBtn[i].style.WebkitAnimation = "fadeOut";
+    }
+    setTimeout(()=>{
+      for(let i = 0; i < childrenBtn.length; i++){
+        childrenBtn[i].style.display = "none";
+      }
+      goNext(++qIdx);//다음질문으로 넘어가야지
+    },450)
+  },false);
+}
+
+
 function goNext(qIdx){
+  if(qIdx+1 === endPoint){
+    goResult();
+    return;
+  }
   let q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
   for (let i in qnaList[qIdx].a){
-    addAnswer(qnaList[qIdx].a[i].answer);
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
   }
+  let status = document.querySelector('.statusBar');
+  status.style.width = (100/endPoint) * (qIdx+1) + '%';
+
 }
 
 //시작하기 버튼 클릭 후 천천히 Q&A화면으로 넘어가기
