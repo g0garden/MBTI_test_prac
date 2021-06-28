1f8373cc8c5abbbeb3e5b45e0babc5f98d091cc7
g0garden <58612140+g0garden@users.noreply.github.com>
2021-06-28 18:17:40 +0900

    data처리 : 어떻게 불러와서 어떻게 그려줄지

--- js/start.js
+++ js/start.js
@@ -1,11 +1,21 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
-// function goNext(qIdx){
-//   let q = document.querySelector('.qBox');
-//   q.innerHTML = qnaList[qIdx].q;
-//   for (let i in qnaList[a]);
-// }
+//data에서 q순서에 맞는 answer들 버튼으로 보여주기
+function addAnswer(answerText){
+  let a = document.querySelector('.answerBox');
+  let answer = document.createElement('button');
+  a.appendChild(answer)
+  answer.innerHTML = answerText;
+}
+
+function goNext(qIdx){
+  let q = document.querySelector('.qBox');
+  q.innerHTML = qnaList[qIdx].q;
+  for (let i in qnaList[qIdx].a){
+    addAnswer(qnaList[qIdx].a[i].answer);
+  }
+}
 
 //시작하기 버튼 클릭 후 천천히 Q&A화면으로 넘어가기
 function begin(){
